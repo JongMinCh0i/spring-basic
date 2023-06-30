0345ee7169c9c92349d764dcbfae973b226375fb
JongMinCh0i <wonderland314592@gmail.com>
2023-07-01 03:48:30 +0900

    Ch2.스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/com/example/core/OrderApp.java
+++ src/main/java/com/example/core/OrderApp.java
@@ -3,16 +3,21 @@ package com.example.core;
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
 import com.example.core.member.MemberService;
-import com.example.core.member.MemberServiceImpl;
 import com.example.core.order.Order;
 import com.example.core.order.OrderService;
-import com.example.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
