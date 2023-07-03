1f3b79b7bbd0603e64cfdbd25a78bc14d3a3e825
JongMinCh0i <wonderland314592@gmail.com>
2023-07-03 17:53:34 +0900

    Ch6.6 조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/com/example/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/core/discount/RateDiscountPolicy.java
@@ -3,7 +3,6 @@ package com.example.core.discount;
 import com.example.core.annotation.MainDiscountPolicy;
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
