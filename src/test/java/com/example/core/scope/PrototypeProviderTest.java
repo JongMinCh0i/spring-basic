7b39a237e9b3aca03473bbf90f8024df9610740b
JongMinCh0i <wonderland314592@gmail.com>
2023-07-04 17:17:08 +0900

    Ch8.3 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/com/example/core/scope/PrototypeProviderTest.java
+++ src/test/java/com/example/core/scope/PrototypeProviderTest.java
@@ -4,29 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 
-public class SingletonWithPrototypeTest1 {
-
-
-    @Test
-    void PrototypeFind() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
-
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
-    }
+public class PrototypeProviderTest {
 
     @Test
-    void singletonClientUsePrototype() {
+    void providerTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -35,29 +21,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
 
     static class ClientBean {
 
-//        private final PrototypeBean prototypeBean; // 생성 시점에 1회 주입
+        // 스프링 컨테이너 자체를 가져와서 그 속에서 찾아온다. : DL (Dependency Lookup)
+        // -> 스프링 컨테이너에 종속적 (단위 테스트 어려움)
 //
 //        @Autowired
-//        ClientBean(PrototypeBean prototypeBean) {
-//            this.prototypeBean = prototypeBean;
-//        }
+//        private ApplicationContext ac;
 
         @Autowired
-        private ObjectProvider<PrototypeBean> provider;
-
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = provider.getObject();
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
+
     }
 
     @Scope("prototype")
